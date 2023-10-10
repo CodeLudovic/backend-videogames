00e01ff63d485fe0dab83533fbfaad3cb0901ae9
Daniel Ospina Ramirez <danielospinar@gmail.com>
2023-10-10 13:35:05 -0500

    Deployment

--- index.js
+++ index.js
@@ -20,10 +20,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { Videogame } = require("./src/db.js");
-
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
 	server.listen(3001, () => {
-		console.log("%s listening at 3001");
+		console.log(`%s listening at ${port}`);
 	});
 });
